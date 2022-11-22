8b93a16d270e81e0ab0877557fc435d222b51b34
Nick <nic7ven@gmail.com>
2022-11-22 20:01:40 +0400

    first commit in Vue

---
+++ src/components/BrandsSection.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "BrandsSection"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/RegisterForm.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+main.main
+  .auth
+    .auth__container
+      .auth__body
+        .auth__img
+          img(src="@/assets/img/icons/ID.png", alt="WILDCHERRY ID")
+        .auth__title
+          h2 Вход в аккаунт
+        .auth__form.form
+          .form__item
+            input.form__input(type="text", required)
+            label.form__label Логин
+          .form__item
+            input.form__input(type="password", required)
+            label.form__label Пароль
+          .form__item
+            button.btn.form__button Войти в аккаунт
+          .form__item
+            a(href="#").form__link Регистрация
+</template>
+
+<script>
+export default {
+  name: "RegisterComponent",
+};
+</script>
+
+<style scoped></style>
